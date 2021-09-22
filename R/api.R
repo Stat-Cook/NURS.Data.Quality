1e8f5cd0cca2a67864b6e7a3e33ba444cfc65778
Rob <robert.cook@bcu.ac.uk>
2021-09-22 10:20:50 +0100

    Add api function

---
+++ R/api.R
@@ -0,0 +1,18 @@
+data.quality <- function(data) {
+  #' Data quality measures
+  #' 
+  #' Perform missing and misscoding checks on supplied data set
+  #' 
+  #' @param data Data frame to perform data quality checks on.
+  #' 
+  #' @return Data frame summarizing data quality metrics.
+  tibble(
+    Variable = colnames(data),
+    `Singular data` = compare_frame_frequencies(data),
+    `Too many distinct values` = compare_frame_classes(data),
+    `High Missing Rate` = average_missing_heuristic(data),
+    `Missing ROC` = missing_mine(data)
+  )
+}
+
+
