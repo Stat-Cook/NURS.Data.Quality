01a12166fa35e9c2efa29a3793bf1388eb06798b
Rob <robert.cook@bcu.ac.uk>
2021-09-29 13:57:37 +0100

    Split mutual_info into two function calls and implement column_info_func

--- R/mutual_info.R
+++ R/mutual_info.R
@@ -8,6 +8,28 @@ encode_values <- function(values){
   as.numeric(as.factor(values))
 }
 
+discrete.data <- function(data, ...){
+  #' Discretize data after encoding factors
+  #' 
+  #' Converts non-numeric data to factors and discretizes all features.
+  #' 
+  #' @param data The data set to be discretized. 
+  #' @param ... Extra arguments to pass to the infotheo::discretize call.
+  #' 
+  #' @export
+  #' 
+  copied <- data
+  numeric_columns <- sapply(copied, is.numeric)
+  factor_columns <- colnames(copied[!numeric_columns])
+  
+  for (name in factor_columns){
+    values <- unlist(copied[, name])
+    copied[, name] <- encode_values(values)
+  }
+  
+  infotheo::discretize(copied, ...)
+}
+
 mutual_info <- function(data){
   #' Calculate mutual information between pairs of variables in a data frame
   #' 
@@ -15,18 +37,9 @@ mutual_info <- function(data){
   #' 
   #' @return matrix (k by k) of mutual information.  Diagonal represents total information a varaible has.
   #' @export
-  copied <- data
-  numeric_columns <- sapply(copied, is.numeric)
-  factor_columns <- colnames(copied[!numeric_columns])
-
-  for (name in factor_columns){
-    values <- unlist(copied[, name])
-    copied[, name] <- encode_values(values)
-  }
+  disc.df <- discrete.data(data)
+  K <- dim(data)[2]
   
-  disc.df <- infotheo::discretize(copied)
-  K <- dim(copied)[2]
-
   info <- sapply(
     1:K, 
     function(i) sapply(
@@ -35,9 +48,33 @@ mutual_info <- function(data){
     )
   )
   
-  rownames(info) <- colnames(copied)
-  colnames(info) <- colnames(copied)
+  rownames(info) <- colnames(data)
+  colnames(info) <- colnames(data)
   
   info
+}
+
+column_info_func <- function(col1, col2){
+  #' Function factory for mutual information.
+  #' 
+  #' Creates a function to be called in the tidyverse that calculates 
+  #' mutual information between variables of a data set.  Intended to be used as 
+  #' part of a group_by(...) %>% group_map(column_info_func(...)) workflow. 
+  #' 
+  #' @param col1 Name of the first column
+  #' @param col2 Name of the second column
+  #' 
+  #' @examples 
+  #' ChickWeight %>% group_by(Diet)  %>% 
+  #'     group_map(column_info_func("Chick", "weight")) %>% 
+  #'     do.call(rbind, .)
+  #' 
+  #' @export
+  #' 
+  f <- function(data, ...){
+    args <- c(...)
+    c(unlist(args), infotheo::mutinformation(data[col1], data[col2]))
+  }
+  f
 }
 
