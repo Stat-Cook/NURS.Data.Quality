7e368f845ec5a3e61df550637d31a112ba2b22b5
Rob <robert.cook@bcu.ac.uk>
2021-10-25 10:41:20 +0100

    Implement unique2clipboard function

--- R/utilities.R
+++ R/utilities.R
@@ -2,7 +2,8 @@ blank_result <- function(data){
   #' Generate a blank result vector from a data frame
   #' 
   #' @param data The data frame to template the result vector from.
-
+  #' @export
+  
   K <- dim(data)[2]
   result <- rep(NA, K)
   names(result) <- colnames(data)
@@ -16,10 +17,27 @@ apply_to_dataframe <- function(data, column_function){
   #' @param column_function Function to be applied to each varaible.
   #' 
   #' @return Vector of results.
+  #' @export
   result <- blank_result(data)
   for (name in names(result)){
     values <- data[,name]
     result[name] <- column_function(values)
   }
   result
+}
+
+
+
+unique2clipboard <- function(values, column_name = NA){
+  #' Send values to clipboard
+  #' 
+  #' Finds all unique values of the vector given, concatenates with new lines and 
+  #' sends to the clipboard.  Can be pasted into excel as a column.
+  #' 
+  #' @param values A vector of values to be processed
+  #'
+  #' @export
+  uni <- unique(values)
+  string <- paste(uni, collapse="\n")
+  writeClipboard(string)
 }
