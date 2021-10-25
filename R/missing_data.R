4cef4d41475882fba1663df2701b82babd85b2a8
Rob <robert.cook@bcu.ac.uk>
2021-10-25 10:45:57 +0100

    Add @export command

--- R/missing_data.R
+++ R/missing_data.R
@@ -7,6 +7,7 @@ is.missing <- function(values, missing_types=MISSING_TYPES){
   #' @param missing_types  Vector of values classified as missing.
   #' 
   #' @return Vector of which values are 'missing'
+  #' @export
   
   sapply(values, function(i) i %in% missing_types)
 }
@@ -18,7 +19,7 @@ average_missing <- function(data, missing_types=MISSING_TYPES){
   #' @param missing_type Vector of values classified as missing.
   #' 
   #' @return Vector of prevalence of missing values.
-
+  
   missing_func <- function(values) mean(is.missing(values, missing_types))
   apply_to_dataframe(data, missing_func)
 }
@@ -30,12 +31,10 @@ average_missing_heuristic <- function(data, limit=0.1, missing_types=MISSING_TYP
   #' @param limit Allowable rate of missingness, valued [0, 1] 
   #' @param missing_types [optional] vector of valuse to define as missing.
   #' 
-  #' @return Boolean-vector - FALSE: missing more data than the limit.
+  #' @return Boolean-vector - TRUE: missing more data than the limit.
+  #' @export
   
-  average_missing(data, missing_types) < limit
+  average_missing(data, missing_types) > limit
   
 }
-
-
-
 
