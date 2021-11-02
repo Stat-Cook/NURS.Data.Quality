288c45ee1cb9630046a92596f51e22e3b85fb3bf
Rob <robert.cook@bcu.ac.uk>
2021-11-02 14:02:01 +0000

    Implement grouped proportions

---
+++ R/crosstab.proportion.R
@@ -0,0 +1,27 @@
+proportions.frame <- function(values, ...){
+  #' 
+  #'
+  #'
+  values <- unlist(values)
+  uni <- unique(values)
+  results <- c()
+  for (i in uni){
+    results <- c(results, mean(values == i))
+  }
+  
+  frame <- data.frame(value = 100*results, name=uni)
+  frame
+}
+
+crosstab.proportion <- function(data, target, group_var){
+  #'
+  #' @export
+  grps <- data %>% group_by({{group_var}})
+  joined <- grps %>% select({{target}}) %>% 
+    group_map(proportions.frame) %>% reduce(full_join, by="name")
+  joined.vals <- joined %>% select(-name)
+  colnames(joined.vals) <-  grps %>% select(A) %>% group_map(~ .y) %>% unlist()
+  rownames(joined.vals) <- joined$name
+  
+  joined.vals  %>% t()
+}
